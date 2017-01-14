5699388a82d804f691901632b75b2f9f9603256f
Amit Patel <pamit@thoughtworks.com>
2017-01-14 15:44:25 +0530

    Android client QR Scanner code

--- AndroidClient/BarcodeReaderSample/app/src/main/java/com/varvet/barcodereadersample/MainActivity.java
+++ AndroidClient/BarcodeReaderSample/app/src/main/java/com/varvet/barcodereadersample/MainActivity.java
@@ -7,27 +7,44 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.google.android.gms.common.api.CommonStatusCodes;
 import com.google.android.gms.vision.barcode.Barcode;
 import com.varvet.barcodereadersample.barcode.BarcodeCaptureActivity;
+import com.varvet.barcodereadersample.camera.CartItem;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
     private static final int BARCODE_READER_REQUEST_CODE = 1;
 
-    private TextView mResultTextView;
+    private ListView listViewCartItem;
+    private Button buttonScanBarCode,buttonAdd,buttonCheckOut;
+    private EditText editTextBarCode;
+    AdapterProductList adapterProductList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mResultTextView = (TextView) findViewById(R.id.result_textview);
+        editTextBarCode = (EditText) findViewById(R.id.editText_barCode);
 
-        Button scanBarcodeButton = (Button) findViewById(R.id.scan_barcode_button);
-        scanBarcodeButton.setOnClickListener(new View.OnClickListener() {
+        buttonScanBarCode = (Button) findViewById(R.id.scan_barcode_button);
+        listViewCartItem =(ListView)findViewById(R.id.listView_item);
+        buttonAdd=(Button)findViewById(R.id.add_button);
+        buttonCheckOut=(Button)findViewById(R.id.checkout_button);
+
+        ArrayList<CartItem> arrayListCart=new ArrayList<CartItem>();
+        adapterProductList=new AdapterProductList(this,R.layout.cart_item_view, arrayListCart);
+        adapterProductList.add(arrayListCart);
+        listViewCartItem.setAdapter(adapterProductList);
+        buttonScanBarCode.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), BarcodeCaptureActivity.class);
@@ -43,8 +60,8 @@ public class MainActivity extends AppCompatActivity {
                 if (data != null) {
                     Barcode barcode = data.getParcelableExtra(BarcodeCaptureActivity.BarcodeObject);
                     Point[] p = barcode.cornerPoints;
-                    mResultTextView.setText(barcode.displayValue);
-                } else mResultTextView.setText(R.string.no_barcode_captured);
+                    editTextBarCode.setText(barcode.displayValue);
+                } else editTextBarCode.setText(R.string.no_barcode_captured);
             } else Log.e(LOG_TAG, String.format(getString(R.string.barcode_error_format),
                     CommonStatusCodes.getStatusCodeString(resultCode)));
         } else super.onActivityResult(requestCode, resultCode, data);
